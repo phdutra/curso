2943400ff2a6bc4c0a26adeb6dba6063b12addac
phdutra <raphaelemdutra@gmail.com>
2016-07-20 20:32:05 -0300

    Inicial comit

---
+++ Quiron.LojaVirtual.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quiron.LojaVirtual.Web.Startup))]
+namespace Quiron.LojaVirtual.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
